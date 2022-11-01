4572fdba67cee38df55788d23de3deb22c5d96c3
michnhokn <michael_scheurich@posteo.de>
2022-11-01 10:20:37 +0100

    Adds git init to setup command

--- site/commands/setup.php
+++ site/commands/setup.php
@@ -7,6 +7,19 @@ return [
         $input = $cli->confirm('Execute Setup?');
 
         if ($input->confirmed()) {
+            $gitUrl = $cli->prompt('Project Git Repository URL:');
+
+            $initCommand = "git init --initial-branch=main -q";
+            $connectCommand = "git remote add origin $gitUrl";
+
+            if (shell_exec($initCommand) !== null) {
+                $cli->success("Init git repository!");
+            }
+
+            if (shell_exec($connectCommand) !== null) {
+                $cli->success("Added origin for git repository!");
+            }
+
             $cli->success("Done!");
         } else {
             $cli->info("Skipped setup!");
